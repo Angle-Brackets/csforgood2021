c18c2af463187eaa55ba5df9a23d2d382d11c72a
Angle-Brackets <superstarmario12@gmail.com>
2021-03-03 01:42:20 -0500

    Finalized Infobox styling and contents

--- src/components/Infobox.js
+++ src/components/Infobox.js
@@ -1,10 +1,10 @@
 import React, {useState} from 'react'
-import {Button, notification, Typography} from 'antd'
-import {QuestionOutlined, GithubFilled} from '@ant-design/icons'
+import {Button, notification, Typography, Space} from 'antd'
+import {QuestionOutlined, GithubFilled, FileExcelFilled} from '@ant-design/icons'
 import 'antd/lib/button/style/index.css';
 import 'antd/lib/notification/style/index.css';
 
-const {Link, Paragraph} = Typography
+const {Link, Paragraph, Title} = Typography
 
 const Infobox = () => {
       const close = () => {
@@ -21,7 +21,14 @@ const Infobox = () => {
         notification.open({
           message: 'Information',
           description:
-            <Typography>This application was brought to you by Ankit Joju and Joshua Galvez for the 2021 CSForGood Competition. The link to the github has been provided for people to study and/or for people to laugh at. Enjoy! <Paragraph><Link href={"https://github.com/Angle-Brackets/csforgood2021"}><GithubFilled/></Link></Paragraph></Typography>,
+            <Typography>
+              This application was brought to you by Ankit Joju and Joshua Galvez for the 2021 CSForGood Competition. The link to the github has been provided for people to study and/or for people to laugh at. The datasets we utilized to create this prototype have also been provided below, consisting of data from 2019-2020 that we felt would be the best to list here, rather than just copying it verbatim. Enjoy! 
+              <Paragraph style={{fontSize: "2rem"}}>
+                <Link href={"https://github.com/Angle-Brackets/csforgood2021"} style={{textDecorationLine: "none", color:"#4B90FC"}}><GithubFilled/>                   </Link>
+                <Link href={"https://blog.rezscore.com/top-100-undergraduate-majors-2019-c9cc32b02bfc"} style={{textDecorationLine: "none", color:"#4B90FC"}}><FileExcelFilled/>                   </Link>
+                <Link href={"https://www.matchcollege.com/top-majors"} style={{textDecorationLine: "none", color:"#4B90FC"}}><FileExcelFilled/>                   </Link>
+              </Paragraph>
+            </Typography>,
           key,
           duration: 0,
           icon: <QuestionOutlined style={{ color: '#108ee9' }}/>,
